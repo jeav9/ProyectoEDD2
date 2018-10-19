cc395290297f28f462de6f29ae85eb1bda2236ee
Jorge Alvarez <jorge.alvarez@unitec.edu>
2018-10-19 15:43:54 -0600

    Arreglar diseño

--- ProyectoEDD2/Form1.Designer.cs
+++ ProyectoEDD2/Form1.Designer.cs
@@ -30,13 +30,11 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
-<<<<<<< HEAD
+
             this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
             this.toolStrip1.SuspendLayout();
-=======
-            this.button1 = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
->>>>>>> Elias
+
+        
             this.SuspendLayout();
             // 
             // toolStrip1
@@ -47,7 +45,7 @@
             this.toolStripButton1});
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
-<<<<<<< HEAD
+
             this.toolStrip1.Size = new System.Drawing.Size(103, 577);
             this.toolStrip1.TabIndex = 0;
             this.toolStrip1.Text = "toolStrip1";
@@ -61,36 +59,18 @@
             this.toolStripButton1.Size = new System.Drawing.Size(100, 44);
             this.toolStripButton1.Text = "Crear Archivo";
             this.toolStripButton1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
-=======
+
             this.toolStrip1.Size = new System.Drawing.Size(26, 534);
             this.toolStrip1.TabIndex = 0;
             this.toolStrip1.Text = "toolStrip1";
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(301, 71);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(310, 114);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 24);
-            this.comboBox1.TabIndex = 2;
->>>>>>> Elias
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(873, 534);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.button1);
+  
             this.Controls.Add(this.toolStrip1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -104,12 +84,8 @@
         #endregion
 
         private System.Windows.Forms.ToolStrip toolStrip1;
-<<<<<<< HEAD
         private System.Windows.Forms.ToolStripButton toolStripButton1;
-=======
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.ComboBox comboBox1;
->>>>>>> Elias
+
     }
 }
 
