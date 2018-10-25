3658b4be46c327f641aa546b7e1cb4c96530552b
Jorge Alvarez <jorge.alvarez@unitec.edu>
2018-10-24 20:38:57 -0600

    Estadon de ventana

--- ProyectoEDD2/Formularios/Menu.Designer.cs
+++ ProyectoEDD2/Formularios/Menu.Designer.cs
@@ -64,7 +64,9 @@
             this.IsMdiContainer = true;
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Menu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Menu Principal";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.toolStrip1.ResumeLayout(false);
             this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
