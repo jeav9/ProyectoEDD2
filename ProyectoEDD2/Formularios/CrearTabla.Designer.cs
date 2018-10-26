98f99220ce9751ff24fbbcc5eef275239450962e
Jorge Alvarez <jorge.alvarez@unitec.edu>
2018-10-25 21:59:46 -0600

    Leer encabezado de tabla

--- ProyectoEDD2/Formularios/CrearTabla.Designer.cs
+++ ProyectoEDD2/Formularios/CrearTabla.Designer.cs
@@ -35,17 +35,15 @@
             this.btnLeer = new System.Windows.Forms.Button();
             this.btnAgregar = new System.Windows.Forms.Button();
             this.textBox2 = new System.Windows.Forms.TextBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.TipoDato = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Tamano = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Tamano = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
@@ -67,117 +65,95 @@
             this.splitContainer1.Panel1.Controls.Add(this.btnLeer);
             this.splitContainer1.Panel1.Controls.Add(this.btnAgregar);
             this.splitContainer1.Panel1.Controls.Add(this.textBox2);
-            this.splitContainer1.Panel1.Controls.Add(this.comboBox1);
             this.splitContainer1.Panel1.Controls.Add(this.textBox1);
             this.splitContainer1.Panel1.Controls.Add(this.label4);
-            this.splitContainer1.Panel1.Controls.Add(this.label3);
             this.splitContainer1.Panel1.Controls.Add(this.label2);
             this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             // 
             // splitContainer1.Panel2
             // 
             this.splitContainer1.Panel2.Controls.Add(this.dataGridView1);
             this.splitContainer1.Size = new System.Drawing.Size(1200, 630);
-            this.splitContainer1.SplitterDistance = 525;
+            this.splitContainer1.SplitterDistance = 607;
             this.splitContainer1.TabIndex = 0;
             // 
             // btnSalir
             // 
-            this.btnSalir.Location = new System.Drawing.Point(130, 430);
+            this.btnSalir.Location = new System.Drawing.Point(224, 409);
             this.btnSalir.Name = "btnSalir";
             this.btnSalir.Size = new System.Drawing.Size(184, 30);
-            this.btnSalir.TabIndex = 11;
+            this.btnSalir.TabIndex = 6;
             this.btnSalir.Text = "Salir";
             this.btnSalir.UseVisualStyleBackColor = true;
             this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(130, 391);
+            this.button3.Location = new System.Drawing.Point(224, 357);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(184, 32);
-            this.button3.TabIndex = 10;
+            this.button3.TabIndex = 5;
             this.button3.Text = "Guardar Tabla";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // txtNombre
             // 
-            this.txtNombre.Location = new System.Drawing.Point(170, 48);
+            this.txtNombre.Location = new System.Drawing.Point(224, 43);
             this.txtNombre.Name = "txtNombre";
-            this.txtNombre.Size = new System.Drawing.Size(301, 22);
-            this.txtNombre.TabIndex = 9;
+            this.txtNombre.Size = new System.Drawing.Size(301, 30);
+            this.txtNombre.TabIndex = 0;
             // 
             // btnLeer
             // 
-            this.btnLeer.Location = new System.Drawing.Point(130, 355);
+            this.btnLeer.Location = new System.Drawing.Point(224, 305);
             this.btnLeer.Name = "btnLeer";
             this.btnLeer.Size = new System.Drawing.Size(184, 30);
-            this.btnLeer.TabIndex = 8;
+            this.btnLeer.TabIndex = 4;
             this.btnLeer.Text = "Leer Tabla";
             this.btnLeer.UseVisualStyleBackColor = true;
+            this.btnLeer.Click += new System.EventHandler(this.btnLeer_Click);
             // 
             // btnAgregar
             // 
-            this.btnAgregar.Location = new System.Drawing.Point(130, 314);
+            this.btnAgregar.Location = new System.Drawing.Point(224, 247);
             this.btnAgregar.Name = "btnAgregar";
             this.btnAgregar.Size = new System.Drawing.Size(184, 35);
-            this.btnAgregar.TabIndex = 7;
+            this.btnAgregar.TabIndex = 3;
             this.btnAgregar.Text = "Agregar";
             this.btnAgregar.UseVisualStyleBackColor = true;
             this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(170, 245);
+            this.textBox2.Location = new System.Drawing.Point(224, 168);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(206, 22);
-            this.textBox2.TabIndex = 6;
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "Int",
-            "String",
-            "Double",
-            "Fecha"});
-            this.comboBox1.Location = new System.Drawing.Point(170, 181);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(206, 24);
-            this.comboBox1.TabIndex = 5;
+            this.textBox2.Size = new System.Drawing.Size(206, 30);
+            this.textBox2.TabIndex = 2;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(170, 120);
+            this.textBox1.Location = new System.Drawing.Point(224, 118);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(206, 22);
-            this.textBox1.TabIndex = 4;
+            this.textBox1.Size = new System.Drawing.Size(206, 30);
+            this.textBox1.TabIndex = 1;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(27, 248);
+            this.label4.Location = new System.Drawing.Point(12, 168);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(137, 17);
+            this.label4.Size = new System.Drawing.Size(191, 25);
             this.label4.TabIndex = 3;
             this.label4.Text = "Tamaño de columna";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(74, 184);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(90, 17);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Tipo de Dato";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(101, 123);
+            this.label2.Location = new System.Drawing.Point(112, 123);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(63, 17);
+            this.label2.Size = new System.Drawing.Size(91, 25);
             this.label2.TabIndex = 1;
             this.label2.Text = "Columna";
             // 
@@ -186,7 +162,7 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(46, 48);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(118, 17);
+            this.label1.Size = new System.Drawing.Size(163, 25);
             this.label1.TabIndex = 0;
             this.label1.Text = "Nombre de Tabla";
             // 
@@ -197,32 +173,29 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Nombre,
-            this.TipoDato,
             this.Tamano});
             this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dataGridView1.Location = new System.Drawing.Point(0, 0);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(671, 630);
+            this.dataGridView1.Size = new System.Drawing.Size(589, 630);
             this.dataGridView1.TabIndex = 0;
             // 
             // Nombre
             // 
             this.Nombre.HeaderText = "Nombre";
             this.Nombre.Name = "Nombre";
-            this.Nombre.Width = 150;
-            // 
-            // TipoDato
-            // 
-            this.TipoDato.HeaderText = "Tipo de Dato";
-            this.TipoDato.Name = "TipoDato";
-            this.TipoDato.Width = 180;
+            this.Nombre.Width = 240;
             // 
             // Tamano
             // 
             this.Tamano.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
             this.Tamano.HeaderText = "Tamaño de Columna";
             this.Tamano.Name = "Tamano";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
             // 
             // CrearTabla
             // 
@@ -257,16 +230,14 @@
         private System.Windows.Forms.Button btnLeer;
         private System.Windows.Forms.Button btnAgregar;
         private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
-        private System.Windows.Forms.DataGridViewTextBoxColumn TipoDato;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Tamano;
         private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Tamano;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
--- ProyectoEDD2/Formularios/CrearTabla.cs
+++ ProyectoEDD2/Formularios/CrearTabla.cs
@@ -26,12 +26,42 @@ namespace ProyectoEDD2.Formularios
         {
             textBox1.Text = string.Empty;
             textBox2.Text = string.Empty;
-            comboBox1.Text = string.Empty;        }
+         }
+        void leer()
+        {
+            string[] separador = { "|","||" };
+            openFileDialog1.FileName = string.Empty;
+            openFileDialog1.Filter = "txt file (*.txt)|*.txt";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtNombre.Text = openFileDialog1.FileName;
+                StreamReader reader = new StreamReader(openFileDialog1.FileName);
+                string encabezado = reader.ReadLine();
+                string[] columnas = encabezado.Split(separador, StringSplitOptions.RemoveEmptyEntries);
+                string[] fila=new string[2];
+                int x = 0;
+                for (int i = 1; i <columnas.Length; i+= 2)
+                {
+                    fila[0] = columnas[x];
+                    fila[1] = columnas[i];
+                    this.dataGridView1.Rows.Add(fila);
+                    x=x+2;
+                }
+            }
+        }
+
         private void btnAgregar_Click(object sender, EventArgs e)
         {
-            string[] fila = { textBox1.Text, comboBox1.SelectedItem.ToString(), textBox2.Text };
-            this.dataGridView1.Rows.Add(fila);
-            limpiar();
+            if (textBox1.Text == string.Empty || textBox2.Text == string.Empty)
+            {
+                MessageBox.Show("No puedes dejar campos vacios");
+            }
+            else
+            {
+                string[] fila = { textBox1.Text, textBox2.Text };
+                this.dataGridView1.Rows.Add(fila);
+                limpiar();
+            }
         }
 
         private void btnSalir_Click(object sender, EventArgs e)
@@ -51,9 +81,10 @@ namespace ProyectoEDD2.Formularios
                     StreamWriter sw = new StreamWriter(saveFileDialog1.FileName);
                     for (int i = 0; i < dataGridView1.RowCount; i++)
                     {
-                        sw.WriteLine(dataGridView1.Rows[i].Cells[0].Value.ToString()+"\t"+ dataGridView1.Rows[i].Cells[1].Value.ToString()+"\t"+ dataGridView1.Rows[i].Cells[2].Value.ToString()+"\n");
+                        sw.Write(dataGridView1.Rows[i].Cells[0].Value.ToString()+"|"+ dataGridView1.Rows[i].Cells[1].Value.ToString()+"||");
                     }
                     sw.Close();
+                    dataGridView1.Rows.Clear();
                 }
             }
             else
@@ -61,6 +92,11 @@ namespace ProyectoEDD2.Formularios
                 MessageBox.Show("Falta el nombre del archivo.");
             }
 
+        }
+
+        private void btnLeer_Click(object sender, EventArgs e)
+        {
+            leer();
         }
     }
 }
